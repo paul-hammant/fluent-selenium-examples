c6d57cf1e8dc9212553a390844276a8b6f34a5d8
Paul Hammant <paul@hammant.org>
2013-07-20 15:18:44 -0500

    inline SOut

--- src/test/java/com/paulhammant/fluentSeleniumExamples/WholeSuiteListener.java
+++ src/test/java/com/paulhammant/fluentSeleniumExamples/WholeSuiteListener.java
@@ -5,8 +5,6 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.RunListener;
 import org.seleniumhq.selenium.fluent.monitors.CodaHaleMetricsMonitor;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.concurrent.TimeUnit;
 
 public class WholeSuiteListener extends RunListener {
@@ -15,19 +13,13 @@ public class WholeSuiteListener extends RunListener {
     public void testRunFinished(Result result) throws Exception {
         super.testRunFinished(result);
 
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
         final ConsoleReporter reporter = ConsoleReporter.forRegistry(codahaleMetricsMonitor.getMetrics())
                 .convertRatesTo(TimeUnit.MILLISECONDS)
                 .convertDurationsTo(TimeUnit.MILLISECONDS)
-                .outputTo(new PrintStream(baos))
+                .outputTo(System.out)
                 .build();
 
         reporter.report();
-
-        // ... for assertion
-
-        System.out.println(baos.toString());
 
     }
 
